665dc3b67e39657f35a2eb56d8b0c3994f13c400
PrashannaR <prashanna.rajbhandari1@gmail.com>
2024-09-07 11:51:58 +0530

    chore: tap updated

--- jhilke.rb
+++ jhilke.rb
@@ -1,16 +1,26 @@
 class Jhilke < Formula
-    desc "Jhilke macOS app for controlling monitor brightness"
-    homepage "https://github.com/PrashannaR/Jhilke-macOS"
-    url "https://github.com/PrashannaR/Jhilke-macOS/releases/download/v1.0.0/Jhilke.zip"
-    sha256 "5ef33c4d02fdcc50f9d8984ede955ae3c6ef93ba72b33c70a539fc675c407e43" 
-    license "MIT"
-  
-    def install
-      bin.install "jhilke" 
-    end
-  
-    test do
-      system "#{bin}/jhilke", "--version"
-    end
+  desc "Jhilke macOS app for controlling monitor brightness"
+  homepage "https://github.com/PrashannaR/Jhilke-macOS"
+  url "https://github.com/PrashannaR/Jhilke-macOS/releases/download/v1.0.0/Jhilke.zip"
+  sha256 "5ef33c4d02fdcc50f9d8984ede955ae3c6ef93ba72b33c70a539fc675c407e43"
+  license "MIT"
+
+  def install
+    prefix.install "Jhilke.app"
+
   end
-  
+
+  def caveats
+    <<~EOS
+      Jhilke has been installed in:
+        #{opt_prefix}/Jhilke.app
+
+      To use it, you may want to move it to your /Applications folder:
+        mv #{opt_prefix}/Jhilke.app /Applications
+    EOS
+  end
+
+  test do
+    system "open", prefix/"Jhilke.app"  
+  end
+end
